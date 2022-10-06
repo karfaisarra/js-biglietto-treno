0692c1ffdee06a7c97af8f909798314dda6269b0
karfaisarra <karfaisarra@gmail.com>
2022-10-06 16:00:56 +0200

    add commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,27 @@
 //chiedere all'utente: il numero di chilometri che vuole percorrere.
 const kilometersToGo = prompt ('Inserisci il numero di chilometri che vuoi percorrere');
-console.log(kilometersToGo);
+//console.log(kilometersToGo);
 
 //chiedere all'utente: l'età del passeggero.
 const age = prompt ('Quanti anni hai?');
-console.log(age);
+//console.log(age);
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km).
 const initialPrice = kilometersToGo * 0.21;
-console.log(initialPrice);
-
+//console.log(initialPrice);
+let price;
 //va applicato uno sconto del 20% per i minorenni.
 if (age < 18 ) {
-    const price = initialPrice - (initialPrice * (20 / 100));
-    document.getElementById('prezzo').innerHTML = `Il prezzo da pagare è: ${price}€`;
+    const sconto = 20 / 100;
+    price = initialPrice - (initialPrice * sconto);
+    
 } else if (age >= 65) {
-    const price = initialPrice - (initialPrice * (40 / 100));
-    document.getElementById('prezzo').innerHTML = `Il prezzo da pagare è: ${price}€`;
+    const sconto = 40 / 100;
+    price = initialPrice - (initialPrice * sconto);
+
 } else {
-    document.getElementById('prezzo').innerHTML = `Il prezzo da pagare è: ${initialPrice}€`;
+    price = initialPrice;
+
 }
+
+document.getElementById('prezzo').innerHTML = `Il prezzo da pagare è: ${price}€`;
