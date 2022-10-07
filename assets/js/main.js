c05a9c6c25b05c15d210b9f80324fe9eaf3f4cd8
karfaisarra <karfaisarra@gmail.com>
2022-10-07 10:37:41 +0200

    add commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 //chiedere all'utente: il numero di chilometri che vuole percorrere.
-const kilometersToGo = prompt ('Inserisci il numero di chilometri che vuoi percorrere');
+const kilometersToGo = Number(prompt ('Inserisci il numero di chilometri che vuoi percorrere'));
 //console.log(kilometersToGo);
 
 //chiedere all'utente: l'età del passeggero.
-const age = prompt ('Quanti anni hai?');
+const age = Number( prompt ('Quanti anni hai?') );
 //console.log(age);
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km).
-const initialPrice = kilometersToGo * 0.21;
+const pricePerKm = 0.21;
+const initialPrice = kilometersToGo * pricePerKm;
 //console.log(initialPrice);
 let price;
 //va applicato uno sconto del 20% per i minorenni.
@@ -24,4 +25,11 @@ if (age < 18 ) {
 
 }
 const priceFixed = price.toFixed(2); 
-document.getElementById('prezzo').innerHTML = `Il prezzo da pagare è: ${priceFixed}€`;
+
+// verificazione 
+if (isNaN(priceFixed)){
+    alert('non hai inserito correttamento i km da percorrere! Devi inserie un valore numerico')
+    document.getElementById('prezzo').innerHTML = `Ops! c'è stato un problema! Riprova.`;
+} else {
+    document.getElementById('prezzo').innerHTML = `Il prezzo da pagare è: ${priceFixed}€`;
+}
